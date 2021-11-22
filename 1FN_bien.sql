fe66de891f19a85066acbf48169c308113e87fce
Camilo Sabogal <lobogral@gmail.com>
2021-11-22 10:59:14 -0500

    Mejoro sintaxis con 1FN y agrego PRAGMA

--- 1FN_bien.sql
+++ 1FN_bien.sql
@@ -1,29 +1,33 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE Clientes (
-  id INTEGER PRIMARY KEY,
+  id INTEGER,
   Nombre TEXT,
-  Apellido TEXT
+  Apellido TEXT,
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE Telefonos (
   id_Cliente INTEGER,
   Telefono TEXT,
-  CONSTRAINT fk_Cliente FOREIGN KEY (id_cliente) REFERENCES Clientes (id)
+  FOREIGN KEY (id_cliente) REFERENCES Clientes (id)
 );
 
-INSERT INTO Clientes VALUES (123, "Rachel", "Ingram");
-INSERT INTO Clientes VALUES (456, "James", "Wright");
-INSERT INTO Clientes VALUES (789, "Cesar", "Dure");
+INSERT INTO Clientes VALUES 
+(123, "Rachel", "Ingram"),
+(456, "James", "Wright"),
+(789, "Cesar", "Dure");
 
-INSERT INTO Telefonos VALUES (123, "555-861-2025");
-INSERT INTO Telefonos VALUES (456, "555-403-1659");
-INSERT INTO Telefonos VALUES (456, "555-776-4100");
-INSERT INTO Telefonos VALUES (456, "555-888-6366");
-INSERT INTO Telefonos VALUES (789, "555-808-9633");
-
+INSERT INTO Telefonos VALUES
+(123, "555-861-2025"),
+(456, "555-403-1659"),
+(456, "555-776-4100"),
+(456, "555-888-6366"),
+(789, "555-808-9633");
 
 SELECT Clientes.id, Clientes.Nombre, Clientes.Apellido, Telefonos.Telefono Telefono
 FROM Clientes
 INNER JOIN Telefonos ON Clientes.id=Telefonos.id_cliente;
 
-DROP TABLE Clientes;
 DROP TABLE Telefonos;
+DROP TABLE Clientes;
--- 1FN_mal.sql
+++ 1FN_mal.sql
@@ -1,13 +1,17 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE Cliente (
-  id INTEGER PRIMARY KEY,
+  id INTEGER,
   Nombre TEXT,
   Apellido TEXT,
-  Telefono TEXT
+  Telefono TEXT,
+  PRIMARY KEY(id)
 );
 
-INSERT INTO Cliente VALUES (123, "Rachel", "Ingram", "555-861-2025");
-INSERT INTO Cliente VALUES (456, "James", "Wright", "555-403-1659, 555-776-4100, 555-888-6366");
-INSERT INTO Cliente VALUES (789, "Cesar", "Dure", "555-808-9633");
+INSERT INTO Cliente VALUES 
+(123, "Rachel", "Ingram", "555-861-2025"),
+(456, "James", "Wright", "555-403-1659, 555-776-4100, 555-888-6366"),
+(789, "Cesar", "Dure", "555-808-9633");
 
 SELECT * FROM Cliente;
 DROP TABLE Cliente;
