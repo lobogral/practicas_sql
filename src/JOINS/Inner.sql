766849f1c685b439b8e79a85219222c9906fcc21
Camilo Sabogal <lobogral@gmail.com>
2021-11-29 13:50:10 -0500

    Separo responsabilidades en JOINS

--- src/JOINS/Inner.sql
+++ src/JOINS/Inner.sql
@@ -1,34 +1,3 @@
-CREATE TABLE Departamentos (
-  id INTEGER,
-  nombre TEXT,
-  PRIMARY KEY(id)
-);
-
-CREATE TABLE Empleados (
-  apellido TEXT,
-  idDepartamento INTEGER,
-  PRIMARY KEY(apellido),
-  FOREIGN KEY (idDepartamento) REFERENCES Departamentos(id)
-);
-
-INSERT INTO Departamentos VALUES 
-(31, "Ventas"),
-(33, "Ingeniería"),
-(34, "Producción"),
-(35, "Mercadeo");
-
-INSERT INTO Empleados VALUES 
-("Andrade", 31),
-("Jordán", 33),
-("Steinberg", 33),
-("Róbinson", 34),
-("Zolano", 34),
-("Gaspar", 36);
-
 SELECT Empleados.apellido Empleado, Departamentos.nombre Departamentos
 FROM Empleados
 INNER JOIN Departamentos ON Departamentos.id=Empleados.idDepartamento;
-
-DROP TABLE Departamentos;
-DROP TABLE Empleados;
-
